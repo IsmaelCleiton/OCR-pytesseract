de81937a36a85c4f5bc7715f42e8caade6a4800b
Ismael Cleiton <ismaelcleitonsf@gmail.com>
2023-04-21 19:12:47 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread('imgs/rg.png')
+
+result = pytesseract.image_to_string(img)
+
+print(result)
